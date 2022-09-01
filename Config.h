d16983c015359b24ab361c73471c5a86bedbec7d
lijinjun <2231173990@qq.com>
2022-09-01 18:12:37 +0800

    提交多线程服务器项目demo

---
+++ Config.h
@@ -0,0 +1,8 @@
+#include <string.h>
+
+namespace Config{
+	const int maxConnect = 1;						//最大连接数量
+	const int listenPort = 8888;					//监听端口号
+	const std::string addrString = "192.168.0.104";	//地址
+	const int pollingPthreadNum = 6;				//轮询任务列表线程数量
+}
