f4f179bc2d86f7270fda8608532da087d22f26f3
lijinjun <2231173990@qq.com>
2022-09-15 15:31:54 +0800

    添加shell file

---
+++ Link.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+dir=$(pwd)
+echo "当前路径为:$dir"
+echo
+
+#所需链接
+needLink=" -lpthread -std=c++11 "
+echo "所需链接:$needLink"
+echo
+
+#main文件
+main_dir=" $dir/*.cpp"
+echo "主路径下文件:$main_dir"
+echo
+
+#数据库文件
+DB_dir=" $dir/ClassDB/*.cpp "
+echo "数据库类文件:$DB_dir"
+echo
+
+#线程类文件
+pthread_dir=" $dir/ClassPthread/*.cpp "
+echo "线程类文件:$pthread_dir"
+echo
+
+#定时器文件
+timer_dir=" $dir/ClassTimer/*.cpp "
+echo "定时器文件:$timer_dir"
+echo
+
+#网络TCP类文件
+Net_dir=" $dir/Net/*.cpp "
+echo "网络TCP类文件:$Net_dir"
+echo
+
+all_Dir="$needLink$main_dir$DB_dir$pthread_dir$timer_dir$Net_dir"
+echo "g++执行$all_Dir"
+g++ $all_Dir -o Server.out
+echo
