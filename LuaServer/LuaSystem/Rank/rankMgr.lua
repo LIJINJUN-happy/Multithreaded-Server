a9255943e810ebadd96f77000c174fcd86f4e9dc
lijinjun <2849533742@qq.com>
2023-03-31 19:05:45 +0800

    提交Lua文件

--- LuaServer/LuaSystem/Rank/rankMgr.lua
+++ LuaServer/LuaSystem/Rank/rankMgr.lua
@@ -1,1 +1,14 @@
-print("OK1")
+--每个Actor Uid对应一个coroutine对象
+local actorCoroutineMap = {}
+
+local function Interface()
+end
+
+function Main()
+	if actorCoroutineMap[uid] and type(actorCoroutineMap[uid]) == "thread" and coroutine.status(actorCoroutineMap[uid]) ~= "dead" then
+		return coroutine.resume(actorCoroutineMap[uid],参数)
+	elseif not actorCoroutineMap[uid] or (type(actorCoroutineMap[uid]) == "thread" and coroutine.status(actorCoroutineMap[uid]) == "dead") then
+		actorCoroutineMap[uid] = coroutine.create(Interface)
+		return coroutine.resume(actorCoroutineMap[uid],参数)
+	end
+end
