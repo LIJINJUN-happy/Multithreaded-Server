635ff8b7d11ae63264629a29c2480daceb1d55e0
lijinjun <2849533742@qq.com>
2023-04-11 14:51:18 +0800

    提交logger代码

--- Logger/ClassLogger/ClassLogger.cpp
+++ Logger/ClassLogger/ClassLogger.cpp
@@ -1,1 +1,43 @@
 #include "ClassLogger.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+ClassLogger::ClassLogger()
+{
+	this->writer.flush();
+}
+
+ClassLogger::~ClassLogger()
+{
+	writer.close();
+}
+
+std::ofstream& ClassLogger::Log(std::string type)
+{
+	
+}
+
+std::ofstream& ClassLogger::Log()
+{
+	this->writer << "[ " << pthread_self() << " ] : ";
+	return this->writer;
+}
+
+void ClassLogger::Init()
+{
+	std::string path = Config::LoadCodePathString;
+	path += "Logger/LogInfo/main_Logger.txt";;
+	this->writer.open(path.c_str(),std::fstream::app|std::fstream::out);
+	if (!writer.is_open())
+	{
+		cout << "Logger Files Open Fail" << endl;
+		assert(false);
+	}
+	else
+	{
+		this->Log() << "Logger Init Success " << std::endl;
+		//writer.flush;
+	}
+}
--- Logger/ClassLogger/ClassLogger.h
+++ Logger/ClassLogger/ClassLogger.h
@@ -5,7 +5,17 @@
 
 class ClassLogger
 {
+private:
+	std::ofstream writer;
 
+public:
+	ClassLogger();
+	~ClassLogger();
+
+public:
+	std::ofstream & Log(std::string type);//Lua 日志输出,type为lua模块
+	std::ofstream& Log();				  //日志输出（c++中）
+	void Init();
 };
 
 #endif
