c728fa08c1d753c4287c0a9da2a71f843c45b3c0
lijinjun <2849533742@qq.com>
2023-04-24 15:19:27 +0800

    修改lua格式

--- LuaServer/LuaSystem/Rank/rankMgr.lua
+++ LuaServer/LuaSystem/Rank/rankMgr.lua
@@ -6,7 +6,7 @@ local actorCoroutineMap_ = {}
 --接口容器
 local InterfaceFunMap_ = {}
 local function loadInterFace()
-	InterfaceFunMap_["AddScore"] = RANK.ChangeScoreRankInfo
+    InterfaceFunMap_["AddScore"] = RANK.ChangeScoreRankInfo
 end
 
 --[[
@@ -17,49 +17,48 @@ fun       @处理协议/调用接口
 arg       @参数
 --]]
 local function Interface_(uid, call, called, fun, arg)
-  --判断模块名
-  if _G[called] == nil then
-    print("Called Erro "..tostring(called))
-    return
-  end
-  
-  --判断处理接口
-  if InterfaceFunMap_[fun] and type(InterfaceFunMap_[fun]) == "function" then
-    return InterfaceFunMap_[fun](_G[called], uid, arg)
-  else
-    print("fun Erro "..tostring(fun))
-    return
-  end
+    --判断模块名
+    if _G[called] == nil then
+        print("Called Erro "..tostring(called))
+        return
+    end
+
+    --判断处理接口
+    if InterfaceFunMap_[fun] and type(InterfaceFunMap_[fun]) == "function" then
+        return InterfaceFunMap_[fun](_G[called], uid, arg)
+    else
+        print("fun Erro "..tostring(fun))
+        return
+    end
 end
 
 --交互函数（C++调用此函数进行通信）
 function Main_(uid, call, called, fun, arg)
-	if actorCoroutineMap_[uid] and type(actorCoroutineMap_[uid]) == "thread" and coroutine.status(actorCoroutineMap_[uid]) ~= "dead" then
-		return coroutine.resume(actorCoroutineMap_[uid], uid, call, called, fun, arg)
-	elseif (not actorCoroutineMap_[uid]) or (type(actorCoroutineMap_[uid]) == "thread" and coroutine.status(actorCoroutineMap_[uid]) == "dead") then
-		actorCoroutineMap_[uid] = coroutine.create(Interface_)
-		return coroutine.resume(actorCoroutineMap_[uid], uid, call, called, fun, arg)
-	end
+    if actorCoroutineMap_[uid] and type(actorCoroutineMap_[uid]) == "thread" and coroutine.status(actorCoroutineMap_[uid]) ~= "dead" then
+        return coroutine.resume(actorCoroutineMap_[uid], uid, call, called, fun, arg)
+    elseif (not actorCoroutineMap_[uid]) or (type(actorCoroutineMap_[uid]) == "thread" and coroutine.status(actorCoroutineMap_[uid]) == "dead") then
+        actorCoroutineMap_[uid] = coroutine.create(Interface_)
+        return coroutine.resume(actorCoroutineMap_[uid], uid, call, called, fun, arg)
+    end
 end
 
 --模块初始化函数
 function DoInit_(serPath)
-	--JSON
-	JSON = dofile(serPath .. "LuaServer/luaLib/json.lua")
+    --JSON
+    JSON = dofile(serPath .. "LuaServer/luaLib/json.lua")
 
-	--ScoreRank
-	dofile(serPath .. "LuaServer/LuaSystem/Rank/ScoreRank/scoreRank.lua")
-	
-	loadInterFace()
-	return true
+    --ScoreRank
+    dofile(serPath .. "LuaServer/LuaSystem/Rank/ScoreRank/scoreRank.lua")
+
+    loadInterFace()
+    return true
 end
-
 
 ----------------------------------------------------Moudle-FUN-------------------------------------------------------
 RANK = {}
 
 --获取信息
 function RANK:ChangeScoreRankInfo(uid, arg)
-	print("-------------------i am in RANK")
-	return "","","ACTOR","RANK",uid
+    print("-------------------i am in RANK")
+    return "", "", "ACTOR", "RANK", uid
 end
