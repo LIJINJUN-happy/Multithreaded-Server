a366ad0132c555e31286046df2ec7679b3bcaca4
lijinjun <2849533742@qq.com>
2023-05-06 15:51:30 +0800

    change format about the lua files

--- LuaServer/LuaSystem/Actor/actor.lua
+++ LuaServer/LuaSystem/Actor/actor.lua
@@ -6,8 +6,8 @@ local actorCoroutine_ = nil
 --接口容器
 local InterfaceFunMap_ = {}
 local function loadInterFace()
-    InterfaceFunMap_["AddScore"] = ACTOR.AddScore
-    InterfaceFunMap_["GetScore"] = ACTOR.GetScore
+	InterfaceFunMap_["AddScore"] = ACTOR.AddScore
+	InterfaceFunMap_["GetScore"] = ACTOR.GetScore
 end
 
 --[[
@@ -18,51 +18,51 @@ fun       @处理协议/调用接口
 arg       @参数
 --]]
 local function Interface_(uid, call, called, fun, arg)
-    --判断模块名
-    if _G[called] == nil then
-        print("Called Erro "..tostring(called))
-        return
-    end
+	--判断模块名
+	if _G[called] == nil then
+		print("Called Erro "..tostring(called))
+		return
+	end
 
-    --判断处理接口
-    if InterfaceFunMap_[fun] and type(InterfaceFunMap_[fun]) == "function" then
-        return InterfaceFunMap_[fun](_G[called], uid, arg)
-    else
-        print("fun Erro "..tostring(fun))
-        return
-    end
+	--判断处理接口
+	if InterfaceFunMap_[fun] and type(InterfaceFunMap_[fun]) == "function" then
+		return InterfaceFunMap_[fun](_G[called], uid, arg)
+	else
+		print("fun Erro "..tostring(fun))
+		return
+	end
 end
 
 --交互函数（C++调用此函数进行通信）
 function Main_(uid, call, called, fun, arg)
-    if actorCoroutine_ and type(actorCoroutine_) == "thread" and coroutine.status(actorCoroutine_) ~= "dead" then
-        return coroutine.resume(actorCoroutine_, uid, call, called, fun, arg)
-    elseif (not actorCoroutine_) or (type(actorCoroutine_) == "thread" and coroutine.status(actorCoroutine_) == "dead") then
-        actorCoroutine_ = coroutine.create(Interface_)
-        return coroutine.resume(actorCoroutine_, uid, call, called, fun, arg)
-    end
+	if actorCoroutine_ and type(actorCoroutine_) == "thread" and coroutine.status(actorCoroutine_) ~= "dead" then
+		return coroutine.resume(actorCoroutine_, uid, call, called, fun, arg)
+	elseif (not actorCoroutine_) or (type(actorCoroutine_) == "thread" and coroutine.status(actorCoroutine_) == "dead") then
+		actorCoroutine_ = coroutine.create(Interface_)
+		return coroutine.resume(actorCoroutine_, uid, call, called, fun, arg)
+	end
 end
 
 --模块初始化函数
 function DoInit_(serPath)
-    --JSON
-    JSON = dofile(serPath .. "LuaServer/luaLib/json.lua")
+	--JSON
+	JSON = dofile(serPath .. "LuaServer/luaLib/json.lua")
 
-    --Bag
-    dofile(serPath .. "LuaServer/LuaSystem/Bag/bagMgr.lua")
+	--Bag
+	dofile(serPath .. "LuaServer/LuaSystem/Bag/bagMgr.lua")
 
-    --Email
-    dofile(serPath .. "LuaServer/LuaSystem/Email/email.lua")
+	--Email
+	dofile(serPath .. "LuaServer/LuaSystem/Email/email.lua")
 
-    loadInterFace()
-    return true
+	loadInterFace()
+	return true
 end
 
 ----------------------------------------------------Moudle-FUN-------------------------------------------------------
 
 --用户模块
 ACTOR = {
-    ["score"] = 0
+	["score"] = 0
 }
 
 --获取信息
@@ -75,18 +75,18 @@ end
 
 --添加积分
 function ACTOR:AddScore(uid, arg)
-    local info = JSON:decode(arg)
-    local num = info.score or 0
-    self.score = self.score + num
-    print("添加后当前积分为:", self.score)
-    coroutine.yield(tostring(self.score), "AddScore", "RANK", "ACTOR", tostring(uid))
-    print("RANK BACK-----")
-    return self.score
+	local info = JSON:decode(arg)
+	local num = info.score or 0
+	self.score = self.score + num
+	print("添加后当前积分为:", self.score)
+	coroutine.yield(tostring(self.score), "AddScore", "RANK", "ACTOR", tostring(uid))
+	print("RANK BACK-----")
+	return self.score
 end
 
 --show score
 function ACTOR:GetScore(uid, arg)
-    print("The CurScore is ", self.score)
-    return self.score
+	print("The CurScore is ", self.score)
+	return self.score
 end
 
--- LuaServer/LuaSystem/Room/roomMgr.lua
+++ LuaServer/LuaSystem/Room/roomMgr.lua
@@ -16,38 +16,38 @@ fun       @处理协议/调用接口
 arg       @参数
 --]]
 local function Interface_(uid, call, called, fun, arg)
-    --判断模块名
-    if _G[called] == nil then
-        print("Called Erro "..tostring(called))
-        return
-    end
+	--判断模块名
+	if _G[called] == nil then
+		print("Called Erro "..tostring(called))
+		return
+	end
 
-    --判断处理接口
-    if InterfaceFunMap_[fun] and type(InterfaceFunMap_[fun]) == "function" then
-        return InterfaceFunMap_[fun](_G[called], uid, arg)
-    else
-        print("fun Erro "..tostring(fun))
-        return
-    end
+	--判断处理接口
+	if InterfaceFunMap_[fun] and type(InterfaceFunMap_[fun]) == "function" then
+		return InterfaceFunMap_[fun](_G[called], uid, arg)
+	else
+		print("fun Erro "..tostring(fun))
+		return
+	end
 end
 
 --交互函数（C++调用此函数进行通信）
 function Main_(uid, call, called, fun, arg)
-    if actorCoroutineMap_[uid] and type(actorCoroutineMap_[uid]) == "thread" and coroutine.status(actorCoroutineMap_[uid]) ~= "dead" then
-        return coroutine.resume(actorCoroutineMap_[uid], uid, call, called, fun, arg)
-    elseif (not actorCoroutineMap_[uid]) or (type(actorCoroutineMap_[uid]) == "thread" and coroutine.status(actorCoroutineMap_[uid]) == "dead") then
-        actorCoroutineMap_[uid] = coroutine.create(Interface_)
-        return coroutine.resume(actorCoroutineMap_[uid], uid, call, called, fun, arg)
-    end
+	if actorCoroutineMap_[uid] and type(actorCoroutineMap_[uid]) == "thread" and coroutine.status(actorCoroutineMap_[uid]) ~= "dead" then
+		return coroutine.resume(actorCoroutineMap_[uid], uid, call, called, fun, arg)
+	elseif (not actorCoroutineMap_[uid]) or (type(actorCoroutineMap_[uid]) == "thread" and coroutine.status(actorCoroutineMap_[uid]) == "dead") then
+		actorCoroutineMap_[uid] = coroutine.create(Interface_)
+		return coroutine.resume(actorCoroutineMap_[uid], uid, call, called, fun, arg)
+	end
 end
 
 --模块初始化函数
 function DoInit_(serPath)
-    --JSON
-    JSON = dofile(serPath .. "LuaServer/luaLib/json.lua")
+	--JSON
+	JSON = dofile(serPath .. "LuaServer/luaLib/json.lua")
 
-    loadInterFace()
-    return true
+	loadInterFace()
+	return true
 end
 
 ----------------------------------------------------Moudle-FUN-------------------------------------------------------
