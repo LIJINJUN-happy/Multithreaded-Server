79be12b2f855ed8638d46a860620ba7cf28495dc
lijinjun <2231173990@qq.com>
2023-05-11 23:07:25 +0800

    更新-优化代码

--- ClassPthreadMgr/ClassPthreadMgr.cpp
+++ ClassPthreadMgr/ClassPthreadMgr.cpp
@@ -177,197 +177,198 @@ void *CheckTaskList(void *args)
             pWorkList = *(((Task*)args)->pWorkList);
         }
 
-        if (pWorkList->size() >= 1)
+        if (pWorkList->size() < 1)
         {
-            //从消息列表取出消息包并读取消息包信息
-            MsgPackage* msgPtr = *(pWorkList->begin());
-            pWorkList->erase(pWorkList->begin());
-            bool resCheck = msgPtr->CheckMsgType("Actor");
-            if (resCheck == true)
-            {
-                //判断是否来自用户的socketMsg
-                userOperator = true;
-            }
-            stringMsg = msgPtr->GetCMD();
-            LOG.Log() << "stringMsg = " << stringMsg << std::endl;
+            continue;
+        }
+        //从消息列表取出消息包并读取消息包信息
+        MsgPackage* msgPtr = *(pWorkList->begin());
+        pWorkList->erase(pWorkList->begin());
+        bool resCheck = msgPtr->CheckMsgType("Actor");
+        if (resCheck == true)
+        {
+            //判断是否来自用户的socketMsg
+            userOperator = true;
+        }
+        stringMsg = msgPtr->GetCMD();
+        LOG.Log() << "stringMsg = " << stringMsg << std::endl;
             
-            //执行任务
-            if (stringMsg.size() >= 1)
+        //执行任务
+        if (stringMsg.size() >= 1)
+        {
+
+            Json::Reader reader(Json::Features::strictMode());
+            Json::Value parseData;
+            if (reader.parse(stringMsg.c_str(), parseData))
             {
+                called = parseData.get("Moudle", 0).asString();
+                fun = parseData.get("Protocol", 0).asString();
 
-                Json::Reader reader(Json::Features::strictMode());
-                Json::Value parseData;
-                if (reader.parse(stringMsg.c_str(), parseData))
+                //判断是不是登录或下线或者注册协议（需要注册过以及登录成功的用户才可以生成 ActorLua虚拟机）
+                bool ifSkip = false;//是否跳过LuaVm的交互操作
+                bool removeActorVmWithLogOut = false;
+                if (called == "GATE" && fun == "c_login_request")//登录请求
                 {
-                    called = parseData.get("Moudle", 0).asString();
-                    fun = parseData.get("Protocol", 0).asString();
+                    std::string account = parseData.get("Account", 0).asString();
+                    std::string password = parseData.get("Password", 0).asString();
+                    bool resultLogin = Gate::Login(account, password, dbPtr, msgPtr->GetOperatePtr(), luaVmMgrPtr, msgPtr->GetsockidMapPrt());
+                    if (resultLogin == false) { ifSkip = true; }                                                                    //登录失败要调过虚拟机交互
+                }
+                else if (called == "GATE" && fun == "c_registered_request")//注册请求
+                {
+                    std::string account = parseData.get("Account", 0).asString();
+                    std::string password = parseData.get("Password", 0).asString();
+                    int code = parseData.get("Code", 0).asInt();
+                    bool resRegistered = Gate::Registered(msgPtr->GetOperatePtr(), account, password, code, dbPtr);
+                    ifSkip = true;                                                                                                  //注册必须跳过,因为只有登录成功才可进入对应的用户LuaVm进行交互
+                }
+                else if (called == "GATE" && fun == "c_registered_token_request")//注册码请求
+                {
+                    std::string em = parseData.get("EmailAddress", 0).asString();
+                    bool resJudege = Gate::JudegeEmailBrandNew(em.c_str(), dbPtr, ((Client*)(msgPtr->GetOperatePtr()))->GetClientFd());
+                    if (resJudege == true) { Gate::GetRegisteredToken(msgPtr->GetOperatePtr(), em.c_str()); }
+                    ifSkip = true;                                                                                                  //注册码请求也需要跳过
+                }
+                else if (called == "GATE" && fun == "c_logout") { removeActorVmWithLogOut = true; }//下线
 
-                    //判断是不是登录或下线或者注册协议（需要注册过以及登录成功的用户才可以生成 ActorLua虚拟机）
-                    bool ifSkip = false;//是否跳过LuaVm的交互操作
-                    bool removeActorVmWithLogOut = false;
-                    if (called == "GATE" && fun == "c_login_request")//登录请求
-                    {
-                        std::string account = parseData.get("Account", 0).asString();
-                        std::string password = parseData.get("Password", 0).asString();
-                        bool resultLogin = Gate::Login(account, password, dbPtr, msgPtr->GetOperatePtr(), luaVmMgrPtr, msgPtr->GetsockidMapPrt());
-                        if (resultLogin == false) { ifSkip = true; }                                                                    //登录失败要调过虚拟机交互
-                    }
-                    else if (called == "GATE" && fun == "c_registered_request")//注册请求
-                    {
-                        std::string account = parseData.get("Account", 0).asString();
-                        std::string password = parseData.get("Password", 0).asString();
-                        int code = parseData.get("Code", 0).asInt();
-                        bool resRegistered = Gate::Registered(msgPtr->GetOperatePtr(), account, password, code, dbPtr);
-                        ifSkip = true;                                                                                                  //注册必须跳过,因为只有登录成功才可进入对应的用户LuaVm进行交互
-                    }
-                    else if (called == "GATE" && fun == "c_registered_token_request")//注册码请求
-                    {
-                        std::string em = parseData.get("EmailAddress", 0).asString();
-                        bool resJudege = Gate::JudegeEmailBrandNew(em.c_str(), dbPtr, ((Client*)(msgPtr->GetOperatePtr()))->GetClientFd());
-                        if (resJudege == true) { Gate::GetRegisteredToken(msgPtr->GetOperatePtr(), em.c_str()); }
-                        ifSkip = true;                                                                                                  //注册码请求也需要跳过
-                    }
-                    else if (called == "GATE" && fun == "c_logout") { removeActorVmWithLogOut = true; }//下线
+                caller = "";
+                arg = stringMsg;
+                uid = ((Client*)(msgPtr->GetOperatePtr()))->GetClientUid();
+                while (ifSkip == false)
+                {
+                    /*LOG.Log() << "caller " << caller << std::endl;
+                    LOG.Log() << "called " << called << std::endl;
+                    LOG.Log() << "fun " << fun << std::endl;
+                    LOG.Log() << "uid " << uid << std::endl;
+                    LOG.Log() << "arg " << arg << std::endl << endl;*/
 
-                    caller = "";
-                    arg = stringMsg;
-                    uid = ((Client*)(msgPtr->GetOperatePtr()))->GetClientUid();
-                    while (ifSkip == false)
+                    //首先修正以下called的模块信息
+                    auto it = luaVmMgrPtr->GetLuaMoudleFilesInfoPtr()->GetMoudleInfo()->find(called);
+                    if (it != luaVmMgrPtr->GetLuaMoudleFilesInfoPtr()->GetMoudleInfo()->end())
                     {
-                        /*LOG.Log() << "caller " << caller << std::endl;
-                        LOG.Log() << "called " << called << std::endl;
-                        LOG.Log() << "fun " << fun << std::endl;
-                        LOG.Log() << "uid " << uid << std::endl;
-                        LOG.Log() << "arg " << arg << std::endl << endl;*/
+                        lua_State* L = nullptr;
+                        pthread_mutex_t* vMLock = nullptr;
+                        bool isCallPublicVm = false;
 
-                        //首先修正以下called的模块信息
-                        auto it = luaVmMgrPtr->GetLuaMoudleFilesInfoPtr()->GetMoudleInfo()->find(called);
-                        if (it != luaVmMgrPtr->GetLuaMoudleFilesInfoPtr()->GetMoudleInfo()->end())
+                        if (luaVmMgrPtr->GetLuaMoudleFilesInfoPtr()->judgeMoudleType(called) == 1)//个人模块
                         {
-                            lua_State* L = nullptr;
-                            pthread_mutex_t* vMLock = nullptr;
-                            bool isCallPublicVm = false;
-
-                            if (luaVmMgrPtr->GetLuaMoudleFilesInfoPtr()->judgeMoudleType(called) == 1)//个人模块
+                            if (luaVmMgrPtr->CheckLuaVmIsExistByIndex(uid) == true)
                             {
-                                if (luaVmMgrPtr->CheckLuaVmIsExistByIndex(uid) == true)
-                                {
-                                    LuaBaseVm* vmPtr = luaVmMgrPtr->GetLuaVmByIndex(uid);
-                                    L = vmPtr->GetLuaStatePtr();
-                                    LOG.Log() << "Call Personal LuaVm" << std::endl;
-                                }
-                                else
-                                {
-                                    LOG.Log() << "Personal Lua VM IS Not Build :" << uid << endl;
-                                    break;
-                                }
-                            }
-                            else if(luaVmMgrPtr->GetLuaMoudleFilesInfoPtr()->judgeMoudleType(called) == 2)//公共模块
-                            {
-                                if (luaVmMgrPtr->CheckLuaVmIsExistByIndex(called) == true)
-                                {
-                                    //公共类Vm需要上锁
-                                    LuaBaseVm* vmPtr = luaVmMgrPtr->GetLuaVmByIndex(called);
-                                    vMLock = ((LuaPublicVm*)vmPtr)->GetPublickVmMutex();
-                                    pthread_mutex_lock(vMLock);
-                                    isCallPublicVm = true;
-                                    L = vmPtr->GetLuaStatePtr();
-                                    LOG.Log() << "Call Public LuaVm" << std::endl;
-                                }
-                                else
-                                {
-                                    LOG.Log() << "Public Lua VM IS Not Build :" << called << endl;
-                                    break;
-                                }
+                                LuaBaseVm* vmPtr = luaVmMgrPtr->GetLuaVmByIndex(uid);
+                                L = vmPtr->GetLuaStatePtr();
+                                LOG.Log() << "Call Personal LuaVm" << std::endl;
                             }
                             else
                             {
-                                //LOG.Log() << "Moudle Not Belong To Personal And Public" << called << endl;
+                                LOG.Log() << "Personal Lua VM IS Not Build :" << uid << endl;
                                 break;
                             }
-
-                            //开始操作C++ Lua 交互栈
-                            lua_settop(L, 0);
-                            lua_getglobal(L, "Main_");
-                            lua_pushstring(L, uid.c_str());
-                            lua_pushstring(L, caller.c_str());
-                            lua_pushstring(L, called.c_str());
-                            lua_pushstring(L, fun.c_str());
-                            lua_pushstring(L, arg.c_str());
-                            lua_pcall(L, 5, 6, 0);
-
-                            if (lua_isboolean(L,-6) && lua_toboolean(L,-6) == true)
+                        }
+                        else if(luaVmMgrPtr->GetLuaMoudleFilesInfoPtr()->judgeMoudleType(called) == 2)//公共模块
+                        {
+                            if (luaVmMgrPtr->CheckLuaVmIsExistByIndex(called) == true)
                             {
-                                if (lua_isnil(L, -1))
-                                {
-                                    ifSkip = true;
-                                }
-                                else //需要传递到不同虚拟机
-                                {
-                                    arg = lua_tostring(L, -5);
-                                    fun = lua_tostring(L, -4);
-                                    called = lua_tostring(L, -3);
-                                    caller = lua_tostring(L, -2);
-                                    uid = lua_tostring(L, -1);
-                                    /*LOG.Log() << "type -1 :" << lua_typename(L, lua_type(L, -1)) << std::endl;
-                                    LOG.Log() << "type -2 :" << lua_typename(L, lua_type(L, -2)) << std::endl;
-                                    LOG.Log() << "type -3 :" << lua_typename(L, lua_type(L, -3)) << std::endl;
-                                    LOG.Log() << "type -4 :" << lua_typename(L, lua_type(L, -4)) << std::endl;
-                                    LOG.Log() << "type -5 :" << lua_typename(L, lua_type(L, -5)) << std::endl;*/
-                                }
+                                //公共类Vm需要上锁
+                                LuaBaseVm* vmPtr = luaVmMgrPtr->GetLuaVmByIndex(called);
+                                vMLock = ((LuaPublicVm*)vmPtr)->GetPublickVmMutex();
+                                pthread_mutex_lock(vMLock);
+                                isCallPublicVm = true;
+                                L = vmPtr->GetLuaStatePtr();
+                                LOG.Log() << "Call Public LuaVm" << std::endl;
                             }
-
-                            //判断是否需要解锁（调用了共有Vm才需要解锁）
-                            if (isCallPublicVm == true)
+                            else
                             {
-                                pthread_mutex_unlock(vMLock);
+                                LOG.Log() << "Public Lua VM IS Not Build :" << called << endl;
+                                break;
                             }
                         }
                         else
                         {
-                            //LOG.Log() << "called Moudle Wrong With Finding " << called << endl;
+                            //LOG.Log() << "Moudle Not Belong To Personal And Public" << called << endl;
                             break;
                         }
-                    }
 
-                    //用户下线（放在这里判断是为了让用户在LuaVm处理完下线操作后才进行移除）
-                    if (removeActorVmWithLogOut == true)
+                        //开始操作C++ Lua 交互栈
+                        lua_settop(L, 0);
+                        lua_getglobal(L, "Main_");
+                        lua_pushstring(L, uid.c_str());
+                        lua_pushstring(L, caller.c_str());
+                        lua_pushstring(L, called.c_str());
+                        lua_pushstring(L, fun.c_str());
+                        lua_pushstring(L, arg.c_str());
+                        lua_pcall(L, 5, 6, 0);
+
+                        if (lua_isboolean(L,-6) && lua_toboolean(L,-6) == true)
+                        {
+                            if (lua_isnil(L, -1))
+                            {
+                                ifSkip = true;
+                            }
+                            else //需要传递到不同虚拟机
+                            {
+                                arg = lua_tostring(L, -5);
+                                fun = lua_tostring(L, -4);
+                                called = lua_tostring(L, -3);
+                                caller = lua_tostring(L, -2);
+                                uid = lua_tostring(L, -1);
+                                /*LOG.Log() << "type -1 :" << lua_typename(L, lua_type(L, -1)) << std::endl;
+                                LOG.Log() << "type -2 :" << lua_typename(L, lua_type(L, -2)) << std::endl;
+                                LOG.Log() << "type -3 :" << lua_typename(L, lua_type(L, -3)) << std::endl;
+                                LOG.Log() << "type -4 :" << lua_typename(L, lua_type(L, -4)) << std::endl;
+                                LOG.Log() << "type -5 :" << lua_typename(L, lua_type(L, -5)) << std::endl;*/
+                            }
+                        }
+
+                        //判断是否需要解锁（调用了共有Vm才需要解锁）
+                        if (isCallPublicVm == true)
+                        {
+                            pthread_mutex_unlock(vMLock);
+                        }
+                    }
+                    else
                     {
-                        Gate::RemoveFromSockIdMap(msgPtr->GetOperatePtr(),msgPtr->GetsockidMapPrt(),uid);//先移除SocketMap中的Client*
-                        luaVmMgrPtr->DeleteLuaBaseVm(uid);                                               //再移除LuaVmMap中的Vm*
-                    }
-
-                }
-            }
-
-            //修改任务数量
-            if (userOperator == true && uid.size() > 0)
-            {
-                void* sockmapPtr = msgPtr->GetsockidMapPrt();
-                auto it = ((map<string, Client*>*)sockmapPtr)->find(uid);
-                if (it != ((map<string, Client*>*)sockmapPtr)->end())//防止掉线了,clientPtr指向无效对象，所以需要判断一下
-                {
-                    Client* clientPtr = (Client*)(msgPtr->GetOperatePtr());
-                    clientPtr->UpdateClientTaskNum(-1);
-                    int taskNum = clientPtr->GetClientTaskNum();
-                    LOG.Log() << "Client " << uid << " Task Total Amount: " << taskNum << std::endl;
-                    if (taskNum <= 0)
-                    {
-                        //恢复被操作的线程索引
-                        clientPtr->UpdateWorkPthreadIndex(-1);
+                        //LOG.Log() << "called Moudle Wrong With Finding " << called << endl;
+                        break;
                     }
                 }
-                else
-                {
-                    //LOG.Log() << "Client " << "Is't Not Online" << std::endl;
-                }
-                userOperator = false;
-            }
 
-            //销毁
-            delete msgPtr;
-            //usleep(170000);
+                //用户下线（放在这里判断是为了让用户在LuaVm处理完下线操作后才进行移除）
+                if (removeActorVmWithLogOut == true)
+                {
+                    Gate::RemoveFromSockIdMap(msgPtr->GetOperatePtr(),msgPtr->GetsockidMapPrt(),uid);//先移除SocketMap中的Client*
+                    luaVmMgrPtr->DeleteLuaBaseVm(uid);                                               //再移除LuaVmMap中的Vm*
+                }
+
+            }
         }
+
+        //修改任务数量
+        if (userOperator == true && uid.size() > 0)
+        {
+            void* sockmapPtr = msgPtr->GetsockidMapPrt();
+            auto it = ((map<string, Client*>*)sockmapPtr)->find(uid);
+            if (it != ((map<string, Client*>*)sockmapPtr)->end())//防止掉线了,clientPtr指向无效对象，所以需要判断一下
+            {
+                Client* clientPtr = (Client*)(msgPtr->GetOperatePtr());
+                clientPtr->UpdateClientTaskNum(-1);
+                int taskNum = clientPtr->GetClientTaskNum();
+                LOG.Log() << "Client " << uid << " Task Total Amount: " << taskNum << std::endl;
+                if (taskNum <= 0)
+                {
+                    //恢复被操作的线程索引
+                    clientPtr->UpdateWorkPthreadIndex(-1);
+                }
+            }
+            else
+            {
+                //LOG.Log() << "Client " << "Is't Not Online" << std::endl;
+            }
+            userOperator = false;
+        }
+
+        //销毁
+        delete msgPtr;
+        //usleep(170000);
     }
     //pthread_mutex_unlock(((Task*)args)->lock); //解锁,其实这一步解锁还是不解锁已经无所谓了,可以屏蔽掉
     return NULL;
