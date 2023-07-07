d32c321603685dc30d24b2e4b98fb6449513ad64
lijinjun <2849533742@qq.com>
2023-07-07 20:05:39 +0800

    更新--提交离线处理模块

--- OffLineSystem/OffLineSystem.cpp
+++ OffLineSystem/OffLineSystem.cpp
@@ -1,1 +1,42 @@
 #include "./OffLineSystem.h"
+
+OffLineData::OffLineData()
+{
+	this->offLineDataMap.clear();
+	this->offLineDataMapPtr = &(this->offLineDataMapPtr);
+}
+
+OffLineData::~OffLineData()
+{
+}
+
+std::map<std::string, std::string>* OffLineData::GetOffLineDataMapPtr()
+{
+	return this->offLineDataMapPtr;
+}
+
+bool OffLineData::CheckoutActorOffLineData(std::string uid)
+{
+	auto it = offLineDataMap.find(uid);
+	if (it != offLineDataMap.end())
+	{
+		return true;
+	}
+	return false;
+}
+
+std::string& OffLineData::FindoutActorOffLineData(std::string uid)
+{
+	auto it = offLineDataMap.find(uid);
+	std::string& data = it->second;
+	return data;
+}
+
+void OffLineData::AddData(std::string uid)
+{
+	if (this->CheckoutActorOffLineData(uid) == true)
+	{
+		return;
+	}
+	this->offLineDataMap[uid] = "";
+}
--- OffLineSystem/OffLineSystem.h
+++ OffLineSystem/OffLineSystem.h
@@ -5,4 +5,52 @@
 #include "../GlobalVariable.h"
 
 
-#endif
+//--------------------------BEGIN------离线数据类(即使用户不在线,也可以从这里取出数据)
+typedef struct OffLineData
+{
+public:
+	std::map<std::string, std::string> offLineDataMap; //UID---JSON数据字符串
+	std::map<std::string, std::string>* offLineDataMapPtr;
+
+public:
+	OffLineData();
+	~OffLineData();
+
+	std::map<std::string, std::string>* GetOffLineDataMapPtr();	//返回离线数据容器地址
+	bool CheckoutActorOffLineData(std::string uid);				//判断是否有该用户离线数据存在
+	std::string& FindoutActorOffLineData(std::string uid);		//获取该用户离线数据的引用
+	void AddData(std::string uid);
+
+} OffLineData;
+//--------------------------END------离线数据类(即使用户不在线,也可以从这里取出数据)
+
+
+
+
+
+//--------------------------BEGIN------登录处理数据（用户登录的时候来这里查看）
+typedef struct LoginOffLineMsg
+{
+
+} LoginOffLineMsg;
+//--------------------------END------登录处理数据（用户登录的时候来这里查看）
+
+
+
+
+
+
+//--------------------------BEGIN------离线系统
+class OffLineSystem
+{
+public:
+
+
+protected:
+
+private:
+
+};
+//--------------------------END------离线系统
+
+#endif-
