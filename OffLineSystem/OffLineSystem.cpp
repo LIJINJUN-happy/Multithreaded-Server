2fc5dba34a152e623350ea5bfff80fb6d465f4fa
lijinjun <2231173990@qq.com>
2023-07-07 23:45:35 +0800

    更新--迭代离线数据模块

--- OffLineSystem/OffLineSystem.cpp
+++ OffLineSystem/OffLineSystem.cpp
@@ -40,3 +40,71 @@ void OffLineData::AddData(std::string uid)
 	}
 	this->offLineDataMap[uid] = "";
 }
+
+void OffLineData::UpdateData(std::string uid, std::string jsonData)
+{
+	std::string data = this->FindoutActorOffLineData(uid);
+	data.clear();
+	data.assign(jsonData);
+	return;
+}
+
+LoginOffLineMsg::LoginOffLineMsg()
+{
+	this->loginOffLineMsgMap.clear();
+	this->loginOffLineMsgMapPtr = &(this->loginOffLineMsgMap);
+}
+
+LoginOffLineMsg::~LoginOffLineMsg()
+{
+	return;
+}
+
+std::map<std::string, std::string>* LoginOffLineMsg::GetLoginOffLineMsgMapPtr()
+{
+	return this->loginOffLineMsgMapPtr;
+}
+
+bool LoginOffLineMsg::CheckLoginOffLineData(std::string uid)
+{
+	auto it = this->loginOffLineMsgMap.find(uid);
+	if (it != this->loginOffLineMsgMap.end())
+	{
+		return true;
+	}
+	return false;
+}
+
+std::string& LoginOffLineMsg::GetLoginOffLineData(std::string uid)
+{
+	auto it = loginOffLineMsgMap.find(uid);
+	std::string& data = it->second;
+	return data;
+}
+
+void LoginOffLineMsg::AddLoginOffLineData(std::string uid, std::string data)
+{
+	this->loginOffLineMsgMap[uid] = data;
+	return;
+}
+
+OffLineSystem::OffLineSystem()
+{
+	this->LoginOffLineMsgPtr = new LoginOffLineMsg();
+	this->OffLineDataPtr = new OffLineData();
+}
+
+OffLineSystem::~OffLineSystem()
+{
+	return;
+}
+
+OffLineData* OffLineSystem::GetOffLineDataPtr()
+{
+	return this->OffLineDataPtr;
+}
+
+LoginOffLineMsg* OffLineSystem::GetLoginOffLineMsgPtr()
+{
+	return this->LoginOffLineMsgPtr;
+}
--- OffLineSystem/OffLineSystem.h
+++ OffLineSystem/OffLineSystem.h
@@ -8,7 +8,7 @@
 //--------------------------BEGIN------离线数据类(即使用户不在线,也可以从这里取出数据)
 typedef struct OffLineData
 {
-public:
+private:
 	std::map<std::string, std::string> offLineDataMap; //UID---JSON数据字符串
 	std::map<std::string, std::string>* offLineDataMapPtr;
 
@@ -16,10 +16,12 @@ public:
 	OffLineData();
 	~OffLineData();
 
+public:
 	std::map<std::string, std::string>* GetOffLineDataMapPtr();	//返回离线数据容器地址
 	bool CheckoutActorOffLineData(std::string uid);				//判断是否有该用户离线数据存在
 	std::string& FindoutActorOffLineData(std::string uid);		//获取该用户离线数据的引用
-	void AddData(std::string uid);
+	void AddData(std::string uid);								//创建离线数据
+	void UpdateData(std::string uid, std::string jsonData);		//更新离线数据
 
 } OffLineData;
 //--------------------------END------离线数据类(即使用户不在线,也可以从这里取出数据)
@@ -28,12 +30,25 @@ public:
 
 
 
-//--------------------------BEGIN------登录处理数据（用户登录的时候来这里查看）
+//--------------------------BEGIN------登录待处理数据（用户登录的时候来这里查看）
 typedef struct LoginOffLineMsg
 {
+private:
+	std::map<std::string, std::string> loginOffLineMsgMap;		//UID---待处理数据字符串
+	std::map<std::string, std::string>* loginOffLineMsgMapPtr;
+
+public:
+	LoginOffLineMsg();
+	~LoginOffLineMsg();
+
+public:
+	std::map<std::string, std::string>* GetLoginOffLineMsgMapPtr();	//返回容器地址
+	bool CheckLoginOffLineData(std::string uid);					//判断该用户是否有数据需要在登录后处理（用户不在线的是会存放至此,等上线的时候再处理）
+	std::string& GetLoginOffLineData(std::string uid);				//获取该用户登录所需处理数据的引用
+	void AddLoginOffLineData(std::string uid, std::string data);	//添加登录待处理数据
 
 } LoginOffLineMsg;
-//--------------------------END------登录处理数据（用户登录的时候来这里查看）
+//--------------------------END------登录待处理数据（用户登录的时候来这里查看）
 
 
 
@@ -44,12 +59,16 @@ typedef struct LoginOffLineMsg
 class OffLineSystem
 {
 public:
+	OffLineSystem();
+	~OffLineSystem();
 
-
-protected:
+public:
+	OffLineData* GetOffLineDataPtr();
+	LoginOffLineMsg* GetLoginOffLineMsgPtr();
 
 private:
-
+	OffLineData* OffLineDataPtr;
+	LoginOffLineMsg* LoginOffLineMsgPtr;
 };
 //--------------------------END------离线系统
 
