c8c576dc0b87d53fa30f4028f6dae34ec6d20da8
lijinjun <2849533742@qq.com>
2023-08-07 10:30:34 +0800

    更新--优化打印

--- etc/Config.h
+++ etc/Config.h
@@ -50,31 +50,31 @@ namespace Config
 	const long serOpenTime = 0;					//开服时间
 
 	//线程配置
-	const int pollingPthreadNum = 1; //轮询任务列表线程数量
+	const int pollingPthreadNum = 4;			//轮询任务列表线程数量
 
 	//监听以及epoll配置
-	const int maxConnect = 256;								//最大同时连接列表值（listen 第二参数）
-	const int listenPort = 8899;							//监听端口号(确保该端口开通了防火墙)
-	const std::string addrString = "127.0.0.1";				//地址
-	const int maxSocketfd = 1024;					//最大socket对象监听上限值
-	const int maxEpollEvent = 1024;					//最大epoll监视事件返回个数
-	const int maxReadDataSize = 256;				//读取sockfd数据大小上限值
+	const int maxConnect = 256;					//最大同时连接列表值（listen 第二参数）
+	const int listenPort = 8899;				//监听端口号(确保该端口开通了防火墙)
+	const std::string addrString = "127.0.0.1";	//地址
+	const int maxSocketfd = 1024;				//最大socket对象监听上限值
+	const int maxEpollEvent = 1024;				//最大epoll监视事件返回个数
+	const int maxReadDataSize = 256;			//读取sockfd数据大小上限值
 
 	//数据库配置
 	const std::string host = "localhost";		//主机地址
 	const std::string user = "root";			//用户，例如：root
 	const std::string passwd = "lijinjun123";	//数据库的密码
-	const std::string db = "";				  //要连接的数据库，例如：student
-	const unsigned int port = 3306;			  //端口
-	const unsigned long client_flag = 0;	  //一般为0
-	// const char *unix_socket = NULL;		  //一般为NULL
+	const std::string db = "";					//要连接的数据库，例如：student
+	const unsigned int port = 3306;				//端口
+	const unsigned long client_flag = 0;		//一般为0
+	// const char *unix_socket = NULL;			//一般为NULL
 
 	//监视检测配置
-	const int HeartbeatIntervalTime = 100; //允许距离上次心跳最大时间
-	const int CheckoutIntervalTime = 30;  //监视线程多久检测一次心跳以及线程状态
+	const int HeartbeatIntervalTime = 100;		//允许距离上次心跳最大时间
+	const int CheckoutIntervalTime = 30;		//监视线程多久检测一次心跳以及线程状态
 
 	//定时器线程配置
-	const int timerIntervalTime = 5; //计时器精度秒数
+	const int timerIntervalTime = 5;			//计时器精度秒数
 
 	//项目代码目录（部署时必须修改对应正确项目路径）
 	const std::string LoadCodePathString = "/home/lijinjun/公共/debug_env/";
@@ -84,7 +84,7 @@ namespace Config
 	const long registerCodeOutTimeInterval = 60;//验证码有效秒数
 
 	//LuaGC 间隔
-	const int LuaVMGcIntervalTime = 250;  //LuaVm GC 间隔
+	const int LuaVMGcIntervalTime = 250;		//LuaVm GC 间隔
 
 	const std::string RedisHost = "127.0.0.1";
 	const int RedisPort = 6379;
